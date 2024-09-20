7b4d1f37ab1d4139f021a9ecd45f002bed24da4c
Izushi <shoeshinesamurai@gmail.com>
2024-09-20 11:32:04 -0700

    exercise 29 completed

--- exercises/ex29.js
+++ exercises/ex29.js
@@ -15,3 +15,17 @@ Chocolate donuts cost $2.45 each
 Cider donuts cost $1.59 each
 Boston Cream donuts cost $5.99 each
 */
+
+const donuts = [
+  { type: "Jelly", cost: 1.22 },
+  { type: "Chocolate", cost: 2.45 },
+  { type: "Cider", cost: 1.59 },
+  { type: "Boston Cream", cost: 5.99 }
+];
+
+let summaries = "";
+
+for (let i = 0; i < donuts.length; i++) {
+  summaries = `${donuts[i].type} donuts cost $${donuts[i].cost} each`;
+  console.log(summaries);
+}

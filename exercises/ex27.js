dab5b3bb4d3a60cafff76141bc15162bbc628b73
Izushi <shoeshinesamurai@gmail.com>
2024-09-20 11:31:46 -0700

    exercise 27 completed

--- exercises/ex27.js
+++ exercises/ex27.js
@@ -22,3 +22,28 @@ add a printAccountSummary() method that returns the following account message:
 Welcome!
 Your balance is currently $1000 and your interest rate is 1%.
 */
+
+const savingsAccount = {
+  balance: 1000,
+  interestRatePercent: 1,
+  deposit: function addMoney(amount) {
+    if (amount > 0) {
+      savingsAccount.balance += amount;
+    }
+  },
+  withdraw: function removeMoney(amount) {
+    var verifyBalance = savingsAccount.balance - amount;
+    if (amount > 0 && verifyBalance >= 0) {
+      savingsAccount.balance -= amount;
+    }
+  }
+};
+
+function printAccountSummary(accountInfo) {
+  console.log(
+    `Welcome!
+    Your balance is currently $${accountInfo.balance} and your interest rate is ${accountInfo.interestRatePercent}%.`
+  );
+};
+
+printAccountSummary(savingsAccount);
